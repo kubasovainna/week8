944b69a82c286a55e95254b3d84673a0590a57de
Inna <kubasovainna@mail.ru>
2021-11-09 20:09:17 +0300

    week8

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
         ...CORS
     }
     const isu = 'itmo224658';
+    post = { "id":"1", "title": isu };
     app
         .use(bodyParser.urlencoded({extended:true}))       
         .all('/login/', r => {
             r.res.set(headers).send(isu);
+        })
+        .all('/wordpress', r => {
+            r.res.set(headers).send(post);
         })
         .all('/code/', r => {
             r.res.set(headers)
